75d52eba39a30a0ef0b4be223d5f4aa888c06a56
AngularNinjaAvenger <osayibishop@gmail.com>
2020-04-27 07:46:54 +0100

    pseudo code

---
+++ main.py
@@ -0,0 +1,25 @@
+# add messages to stack 
+
+# store other audios files in a list
+
+# get the total number of audio files
+
+# get the total number of messages
+
+# store the sum of audio file / sum of messages
+
+# create the distributed list
+
+# iterate over the audio files using emnumerators
+
+        # check if the current index % (sum of audio file / sum of messages) equals to 0
+            # check if the message stack is note empty
+                # pop a message from the stack and add it to the distirbuted list
+            # if it's 
+                # break 
+    
+        # add the current audio file to the distributed list
+    
+# iterate over the distributed list usnig an enumerator
+    # rename the curent file
+    # move the file to the desired location
