a7dfd8bcbd6bf77bc685944dcb847a79c538228b
AngularNinjaAvenger <osayibishop@gmail.com>
2020-04-27 07:50:59 +0100

    created stack data structure

---
+++ stack.py
@@ -0,0 +1,15 @@
+class myStack:
+     def __init__(self):
+         self = []
+
+     def isEmpty(self):
+         return self == []
+
+     def push(self, item):
+         self.append(item)
+
+     def pop(self):
+         return self.pop()
+
+     def size(self):
+         return len(self)
